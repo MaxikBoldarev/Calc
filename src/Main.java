1db5ef7c7c1d30e867c1557892cf9f5b1347ae95
Maxim <maxikboldarev13@yandex.ru>
2023-08-28 17:30:08 +0200

    init

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(2, 1);
+        int c = calc.devide.apply(a, b);
+        /**Деление на ноль (Арифметическая ошибка) в devide, так как в переменную b изначально присваивалось значение 0.
+         Заменил передаваемые параметры, получил 1.*/
+
+        int d = calc.multiply.apply(2, 2);
+        int e = calc.pow.apply(3);
+        int f = calc.abs.apply(-2); // Тернальный оператор работает нормально, получаем модуль числа.
+
+        boolean g = calc.isPositive.test(0);
+        /**Возвращает тип boolean, поэтому Consumer не может его принять,
+         так как принимает Integer.
+         Здесь можно воспользоваться напрямую методом println либо сделать еще Consumer для Boolean типов. */
+
+
+        calc.println.accept(c);
+        calc.println.accept(d);
+        calc.println.accept(e);
+        calc.println.accept(f);
+
+        System.out.println(g); //Вариант 1 реализации вывода boolean значение
+        calc.booleanConsumerPrintln.accept(g); //Вариант 2 реализации вывода boolean значение
+    }
+}
